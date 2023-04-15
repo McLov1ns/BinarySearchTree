55253a1a250a64f02d7f5c4e82c4961d295c72eb
суперкомп <@DEMIN-ALEX>
2023-04-15 12:13:04 +0700

    Добавьте файлы проекта.

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace BinarySearchTree
+{
+    using System;
+
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+
+            Console.WriteLine("Enter numbers to add to the binary search tree (press Enter to stop):");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    break;
+
+                int number = int.Parse(input);
+                tree.Add(number);
+            }
+
+            Console.WriteLine("Enter a number to search for:");
+            int searchNumber = int.Parse(Console.ReadLine());
+
+            Node result = tree.Search(searchNumber);
+            if (result != null)
+                Console.WriteLine($"Node with value {searchNumber} found!");
+            else
+                Console.WriteLine($"Node with value {searchNumber} not found.");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Tree
+    {
+        public Node root;
+
+        public Tree()
+        {
+            root = null;
+        }
+
+        public Node Search(int data)
+        {
+            return Search(root, data);
+        }
+
+        private Node Search(Node node, int data)
+        {
+            if (node == null || node.Data == data)
+                return node;
+
+            if (data < node.Data)
+                return Search(node.Left, data);
+            else
+                return Search(node.Right, data);
+        }
+
+        public void Add(int data)
+        {
+            root = Add(root, data);
+        }
+
+        private Node Add(Node node, int data)
+        {
+            if (node == null)
+                return new Node(data);
+
+            if (data < node.Data)
+                node.Left = Add(node.Left, data);
+            else if (data > node.Data)
+                node.Right = Add(node.Right, data);
+
+            return node;
+        }
+    }
+}
